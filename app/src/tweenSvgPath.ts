cd680febd6607ce60c277d54674abc95eb25469f
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2019-12-08 23:21:12 +0100

    working prototype

--- app/src/tweenSvgPath.ts
+++ app/src/tweenSvgPath.ts
@@ -1,6 +1,31 @@
-import animation-frame-delta from animation-frame-delta
+import animationFrameDelta from "animation-frame-delta"
+import TWEEN from "@tweenjs/tween.js"
 
-
-export default function() {
+animationFrameDelta((...a) => {
+  //console.log(a);
   
+})
+
+
+type Segments = (string | number)[][]
+
+export default function(from: Segments | string | SVGPathElement, to: Segments | string, ) {
+  
+}
+
+const testElem = document.querySelector("#test")[0]
+
+export function tweenPreparedSvgPaths(from: Segments, to: Segments | string, update: () => void) {
+  if (from.length !== to.length) throw new Error("Both paths must have equal ammount of points")
+  for (let i = 0; i < from.length; i++) {
+  let start = from[i];
+  let end = to[i];
+  const tween = new TWEEN.Tween(start)
+    .to(end, 1000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate((e) => {
+      testElem.innerText = i + "     " + start.join();
+    })
+    .start();
+  }
 }
--- node_modules/animation-frame-delta/README.md
+++ node_modules/animation-frame-delta/README.md
@@ -1,1 +1,17 @@
-# reqaf
+# Animation frame delta
+
+
+
+## Example
+
+Register a animation loop like so
+
+```js
+import animFrame from "animation-frame-delta"
+
+animFrame((delta, timestamp, absoluteDelta) => {
+  console.log(delta)
+})
+```
+
+To unsubscribe the registered listener simply call `unsubscribe(func)`
